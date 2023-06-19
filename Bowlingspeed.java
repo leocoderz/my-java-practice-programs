7df662364a79136dc82441085ee2d11278778839
natarajaiml <109903331+natarajaiml@users.noreply.github.com>
2023-06-19 18:13:33 +0530

    Create Bowlingspeed.java

---
+++ Bowlingspeed.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //Scanner class
+    Scanner sc = new Scanner(System.in);
+       // double variable
+    double a;
+    a=sc.nextDouble();
+    System.out.printf("%.2f",a);
+    
+        
+    }
+    }
