a314c3c8e547be1e24ae256d70bbe2a4d1a1b9bf
natarajaiml <109903331+natarajaiml@users.noreply.github.com>
2023-06-19 18:12:04 +0530

    Create cricketerageprog.java

---
+++ cricketerageprog.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int age;
+        age=sc.nextInt();
+        
+        System.out.println("The cricketer age is "+age);
+    }
+}
