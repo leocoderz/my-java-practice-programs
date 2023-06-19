0a3742a7d3ac2bf19a108290848c04fe609614f7
natarajaiml <109903331+natarajaiml@users.noreply.github.com>
2023-06-19 18:12:41 +0530

    Update stadiumprog.java

--- stadiumprog.java
+++ stadiumprog.java
@@ -1,5 +1,9 @@
 public class Main{
     public static void main(String args[]){
-        System.out.println("Hello People! Welcome to our desert.");
+        //name varible is creted to store naae of stadium
+        String name ="MECON";
+        String sd = "Stadium";
+        System.out.println(name + "\n"+ sd);
+        
     }
 }
