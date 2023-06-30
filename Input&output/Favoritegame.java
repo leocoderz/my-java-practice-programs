5377cfcfc1f73a3de6b07dff73f6e7215b692c03
natarajaiml <109903331+natarajaiml@users.noreply.github.com>
2023-06-30 14:39:03 +0530

    Add files via upload

---
+++ Input&output/Favoritegame.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String Name;
+        String Game;
+        Name=sc.nextLine();
+        Game=sc.nextLine();
+        System.out.println("Name:");
+        System.out.println("Game:");
+        System.out.println("My name is "+Name+" and I love to play "+Game);
+        
+        
+    }
+}
